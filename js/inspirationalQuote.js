d53dec643f1e845b47f960f7e50ab6f9180284ff
Arthur <arthur.ferrande@gmail.com>
2022-06-23 16:59:20 -0300

    finalizado

--- js/inspirationalQuote.js
+++ js/inspirationalQuote.js
@@ -1,24 +1,24 @@
 const Dia = () => {
     let getDia = new Date();
     let dia = getDia.getDate();
-    let AZ = (dia < 10) ?  "0" + dia : dia
-    return getById("dailyMemoDateNumber").innerText = AZ}
-Dia()
+    let AZ = (dia < 10) ?  "0" + dia : dia;
+    return getById("dailyMemoDateNumber").innerText = AZ;
+}
+Dia();
 
 const DSemana = () => {
     let getDS = new Date().toLocaleString(
         'pt-BR', {month: 'short', 
         weekday: 'short'},
       );
-    console.log(getDS)
     return getById("dailyMemoDay").innerText = getDS;
 }
 DSemana()
 
 const InspQuote = () => {
     fetch("https://type.fit/api/quotes").then(data => {
-    data.json().then(data => {
-        getById("dailyInspirationalQuote").innerText = Randomize(data).text;})
+    data.json().then(dataGET => {
+        getById("dailyInspirationalQuote").innerText = Randomize(dataGET).text;})
     }).catch(err => console.log(err));
 }
 InspQuote()
@@ -26,45 +26,45 @@ InspQuote()
 // https://type.fit/api/quotes
 
 
-/* const setQuoteDate = () => {
-    const dailyMemoDay = getById("dailyMemoDay");
-    const dailyMemoDateNumber = getById("dailyMemoDateNumber");
-    const date = new Date();
-    const dateString = date.toDateString();
-    const dateArray = dateString.split(" ");
-    dailyMemoDay.innerText = `${dateArray[0]} ${dateArray[1]}`
-    dailyMemoDateNumber.innerText = dateArray[2]
-}
+//  const setQuoteDate = () => {
+//     const dailyMemoDay = getById("dailyMemoDay");
+//     const dailyMemoDateNumber = getById("dailyMemoDateNumber");
+//     const date = new Date();
+//     const dateString = date.toDateString();
+//     const dateArray = dateString.split(" ");
+//     dailyMemoDay.innerText = `${dateArray[0]} ${dateArray[1]}`
+//     dailyMemoDateNumber.innerText = dateArray[2]
+// }
 
-===================================================================================****************************
+// ===================================================================================****************************
 
-/*https://crudcrud.com/api/98cf7a11bf7a4a319ea0d20dc27adce1/valorant
+// https://crudcrud.com/api/c539e6e7a78d40d7b1dd477486dbe015
 
-fetch(url, data);*/
+// fetch(url, data);
 
-// const chaveAPI = "98cf7a11bf7a4a319ea0d20dc27adce1";
-// const resource = "pessoas";
-// const id = "62a26ab06f047803e8ae858b"
+// const chaveAPI = "5bc11a92005540d08aae0b3403ef5316";
+// const resource = "valorant";
+// const id = "62a433d96f047803e8ae88ad"
 // const reqURL = `https://crudcrud.com/api/${chaveAPI}/${resource}`
 
 // const body = {
-//     name: "Klebinho",
-//     type: "klebederico"
+//     name: "Valorant",
+//     type: "FPS",
+//     region: "BR"
 // }
 
-// const data = {
+// const datatest = {
 //     method: "GET",
 //     headers: {
 //         "Accept": "application/json",
 //         "Content-Type": "application/json"
 //     },
-//     //body: JSON.stringify(body)
+//     // body: JSON.stringify(body)
 // }
 
-// fetch(reqURL, data).then(d => {
-//     d.json().then(data => {
-//         debugger; 
-//         console.log(data);
+// fetch(reqURL, datatest).then(d => {
+//     d.json().then(datatest => { 
+//         console.log(datatest);
 //     })
 // }).catch(err => console.log(err));
 
