9981cbb4b4c4f875997ebc27a527e73eae8f2055
Arthur <arthur.ferrande@gmail.com>
2022-06-23 17:00:32 -0300

    correção de termos

--- js/tiktactoe.js
+++ js/tiktactoe.js
@@ -4,15 +4,15 @@ let CPUTurno = false;
 
 let tttPont;
 
-const hasX = id => {
+const TemX = id => {
     return getById(id).classList.contains("x");
 }
 
-const hasO = id => {
+const TemO = id => {
     return getById(id).classList.contains("o");
 }
 
-const hasXorO = id => {
+const TemXouO = id => {
     return (
         getById(id).classList.contains("x") || getById(id).classList.contains("o")
     );
@@ -29,51 +29,51 @@ const pontuar = array => {
 
 const XnaBoard = () => {
     const combina = [
-        [!hasXorO(1), hasX(2), hasX(3), 1], // horizontal [ox!=1, x=2, x=3]
-        [hasX(1), !hasXorO(2), hasX(3), 2], // horizontal [x=1, ox!=2, x=3]
-        [hasX(1), hasX(2), !hasXorO(3), 3], // horizontal [x=1, x=2, ox!=3]
+        [!TemXouO(1), TemX(2), TemX(3), 1], 
+        [TemX(1), !TemXouO(2), TemX(3), 2], 
+        [TemX(1), TemX(2), !TemXouO(3), 3],
         
-        [!hasXorO(4), hasX(5), hasX(6), 4], // horizontal [ox!=4, x=5, x=6]
-        [hasX(4), !hasXorO(5), hasX(6), 5], // horizontal [x=4, ox!=5, x=6]
-        [hasX(4), hasX(5), !hasXorO(6), 6], // horizontal [x=4, x=5, ox!=6]
+        [!TemXouO(4), TemX(5), TemX(6), 4], 
+        [TemX(4), !TemXouO(5), TemX(6), 5], 
+        [TemX(4), TemX(5), !TemXouO(6), 6], 
     
-        [!hasXorO(7), hasX(8), hasX(9), 7], // horizontal [ox!=7, x=8, x=9]
-        [hasX(7), !hasXorO(8), hasX(9), 8], // horizontal [x=7, ox!=8, x=9]
-        [hasX(7), hasX(8), !hasXorO(9), 9], // horizontal [x=7, x=8, ox!=9]
+        [!TemXouO(7), TemX(8), TemX(9), 7], 
+        [TemX(7), !TemXouO(8), TemX(9), 8], 
+        [TemX(7), TemX(8), !TemXouO(9), 9], 
     
-        [!hasXorO(1), hasX(4), hasX(7), 1], // vertical [ox!=1, x=4, x=7]
-        [hasX(1), !hasXorO(4), hasX(7), 4], // vertical [x=1, ox!=4, x=7]
-        [hasX(1), hasX(4), !hasXorO(7), 7], // vertical [x=1, x=4, ox!=7]
+        [!TemXouO(1), TemX(4), TemX(7), 1], 
+        [TemX(1), !TemXouO(4), TemX(7), 4], 
+        [TemX(1), TemX(4), !TemXouO(7), 7], 
     
-        [!hasXorO(2), hasX(5), hasX(8), 2], // vertical [ox!=2, x=5, x=8]
-        [hasX(2), !hasXorO(5), hasX(8), 5], // vertical [x=2, ox!=5, x=8]
-        [hasX(2), hasX(5), !hasXorO(8), 8], // vertical [x=2, x=5, ox!=8]
+        [!TemXouO(2), TemX(5), TemX(8), 2], 
+        [TemX(2), !TemXouO(5), TemX(8), 5], 
+        [TemX(2), TemX(5), !TemXouO(8), 8], 
     
-        [!hasXorO(3), hasX(6), hasX(9), 3], // vertical [ox!=3, x=6, x=9]
-        [hasX(3), !hasXorO(6), hasX(9), 6], // vertical [x=3, ox!=6, x=9]
-        [hasX(3), hasX(6), !hasXorO(9), 9], // vertical [x=3, x=6, ox!=9]
+        [!TemXouO(3), TemX(6), TemX(9), 3], 
+        [TemX(3), !TemXouO(6), TemX(9), 6], 
+        [TemX(3), TemX(6), !TemXouO(9), 9], 
     
-        [!hasXorO(1), hasX(5), hasX(9), 1], // diagnal [ox!=1, x=5, x=9]
-        [hasX(1), !hasXorO(5), hasX(9), 5], // diagnal [x=1, ox!=5, x=9]
-        [hasX(1), hasX(5), !hasXorO(9), 9], // diagnal [x=1, x=5, ox!=9]
+        [!TemXouO(1), TemX(5), TemX(9), 1], 
+        [TemX(1), !TemXouO(5), TemX(9), 5], 
+        [TemX(1), TemX(5), !TemXouO(9), 9], 
     
-        [!hasXorO(3), hasX(5), hasX(7), 3], // diagnal [ox!=3, x=5, x=7]
-        [hasX(3), !hasXorO(5), hasX(7), 5], // diagnal [x=3, ox!=5, x=7]
-        [hasX(3), hasX(5), !hasXorO(7), 7], // diagnal [x=3, x=5, ox!=7]
+        [!TemXouO(3), TemX(5), TemX(7), 3], 
+        [TemX(3), !TemXouO(5), TemX(7), 5],
+        [TemX(3), TemX(5), !TemXouO(7), 7],
     ];
     return combina;
 }
 
 const Xlinha = () =>{
     const combina = [
-        [hasX(1), hasX(2), hasX(3)],
-        [hasX(4), hasX(5), hasX(6)],
-        [hasX(7), hasX(8), hasX(9)],
-        [hasX(1), hasX(4), hasX(7)],
-        [hasX(2), hasX(5), hasX(8)],
-        [hasX(3), hasX(6), hasX(9)],
-        [hasX(1), hasX(5), hasX(9)],
-        [hasX(3), hasX(5), hasX(7)],
+        [TemX(1), TemX(2), TemX(3)],
+        [TemX(4), TemX(5), TemX(6)],
+        [TemX(7), TemX(8), TemX(9)],
+        [TemX(1), TemX(4), TemX(7)],
+        [TemX(2), TemX(5), TemX(8)],
+        [TemX(3), TemX(6), TemX(9)],
+        [TemX(1), TemX(5), TemX(9)],
+        [TemX(3), TemX(5), TemX(7)],
     ];
     for(let x = 0; x < combina.length; x++){
         const data = combina[x];
@@ -86,46 +86,46 @@ const Xlinha = () =>{
 
 const OnaBoard = () => {
     const combina = [
-        [!hasXorO(1), hasO(2), hasO(3), 1], // horizontal [ox!=1, x=2, x=3]
-        [hasO(1), !hasXorO(2), hasO(3), 2], // horizontal [x=1, ox!=2, x=3]
-        [hasO(1), hasO(2), !hasXorO(3), 3], // horizontal [x=1, x=2, ox!=3]
+        [!TemXouO(1), TemO(2), TemO(3), 1], 
+        [TemO(1), !TemXouO(2), TemO(3), 2], 
+        [TemO(1), TemO(2), !TemXouO(3), 3], 
         
-        [!hasXorO(4), hasO(5), hasO(6), 4], // horizontal [ox!=4, x=5, x=6]
-        [hasO(4), !hasXorO(5), hasO(6), 5], // horizontal [x=4, ox!=5, x=6]
-        [hasO(4), hasO(5), !hasXorO(6), 6], // horizontal [x=4, x=5, ox!=6]
+        [!TemXouO(4), TemO(5), TemO(6), 4], 
+        [TemO(4), !TemXouO(5), TemO(6), 5], 
+        [TemO(4), TemO(5), !TemXouO(6), 6], 
     
-        [!hasXorO(7), hasO(8), hasO(9), 7], // horizontal [ox!=7, x=8, x=9]
-        [hasO(7), !hasXorO(8), hasO(9), 8], // horizontal [x=7, ox!=8, x=9]
-        [hasO(7), hasO(8), !hasXorO(9), 9], // horizontal [x=7, x=8, ox!=9]
+        [!TemXouO(7), TemO(8), TemO(9), 7], 
+        [TemO(7), !TemXouO(8), TemO(9), 8], 
+        [TemO(7), TemO(8), !TemXouO(9), 9], 
     
-        [!hasXorO(1), hasO(4), hasO(7), 1], // vertical [ox!=1, x=4, x=7]
-        [hasO(1), !hasXorO(4), hasO(7), 4], // vertical [x=1, ox!=4, x=7]
-        [hasO(1), hasO(4), !hasXorO(7), 7], // vertical [x=1, x=4, ox!=7]
+        [!TemXouO(1), TemO(4), TemO(7), 1], 
+        [TemO(1), !TemXouO(4), TemO(7), 4], 
+        [TemO(1), TemO(4), !TemXouO(7), 7], 
     
-        [!hasXorO(2), hasO(5), hasO(8), 2], // vertical [ox!=2, x=5, x=8]
-        [hasO(2), !hasXorO(5), hasO(8), 5], // vertical [x=2, ox!=5, x=8]
-        [hasO(2), hasO(5), !hasXorO(8), 8], // vertical [x=2, x=5, ox!=8]
+        [!TemXouO(2), TemO(5), TemO(8), 2], 
+        [TemO(2), !TemXouO(5), TemO(8), 5],
+        [TemO(2), TemO(5), !TemXouO(8), 8], 
     
-        [!hasXorO(3), hasO(6), hasO(9), 3], // vertical [ox!=3, x=6, x=9]
-        [hasO(3), !hasXorO(6), hasO(9), 6], // vertical [x=3, ox!=6, x=9]
-        [hasO(3), hasO(6), !hasXorO(9), 9], // vertical [x=3, x=6, ox!=9]
+        [!TemXouO(3), TemO(6), TemO(9), 3], 
+        [TemO(3), !TemXouO(6), TemO(9), 6], 
+        [TemO(3), TemO(6), !TemXouO(9), 9], 
     
-        [!hasXorO(1), hasO(5), hasO(9), 1], // diagnal [ox!=1, x=5, x=9]
-        [hasO(1), !hasXorO(5), hasO(9), 5], // diagnal [x=1, ox!=5, x=9]
-        [hasO(1), hasO(5), !hasXorO(9), 9], // diagnal [x=1, x=5, ox!=9]
+        [!TemXouO(1), TemO(5), TemO(9), 1], 
+        [TemO(1), !TemXouO(5), TemO(9), 5], 
+        [TemO(1), TemO(5), !TemXouO(9), 9], 
     
-        [!hasXorO(3), hasO(5), hasO(7), 3], // diagnal [ox!=3, x=5, x=7]
-        [hasO(3), !hasXorO(5), hasO(7), 5], // diagnal [x=3, ox!=5, x=7]
-        [hasO(3), hasO(5), !hasXorO(7), 7], // diagnal [x=3, x=5, ox!=7]
+        [!TemXouO(3), TemO(5), TemO(7), 3], 
+        [TemO(3), !TemXouO(5), TemO(7), 5], 
+        [TemO(3), TemO(5), !TemXouO(7), 7], 
     ];
     return combina;
 }
 
 const BoardCheia = () => {
     const combina = [
-        hasXorO(1), hasXorO(2), hasXorO(3),
-        hasXorO(4), hasXorO(5), hasXorO(6),
-        hasXorO(7), hasXorO(8), hasXorO(9),
+        TemXouO(1), TemXouO(2), TemXouO(3),
+        TemXouO(4), TemXouO(5), TemXouO(6),
+        TemXouO(7), TemXouO(8), TemXouO(9),
     ];
     return !combina.includes(false);
 }
@@ -133,7 +133,7 @@ const BoardCheia = () => {
 const SelJogada = () => {
     const QL = [];
     for(let i = 1; i < 10; i++){
-        if(!hasXorO(i)){
+        if(!TemXouO(i)){
              QL.push(i);
         }
     }
@@ -182,10 +182,9 @@ const JogadaCPU = () => {
     }
 }
 
-//`${VJog}`
 
 const selectSquare = id => {
-    if(!CPUTurno && !hasXorO(id)){
+    if(!CPUTurno && !TemXouO(id)){
         getById(id).classList.add("x");
             if(Xlinha()){
                 PScore("ply")
