af2ecdc1ac142bca609d82b7a4c2d44b65a54a68
Arthur <arthur.ferrande@gmail.com>
2022-06-23 17:00:16 -0300

    correção de strings

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,6 @@
 const resetScores = () => {
     if (getById("mmg").checked) localStorage.setItem("mgHighScore", 0)
-    if (getById("ttt").checked) localStorage.setItem("ticTacToeScore", JSON.stringify({wins: 0, losses: 0}))
+    if (getById("ttt").checked) localStorage.setItem("ticTacToeScore", JSON.stringify({vitórias: 0, derrotas: 0}))
     if (getById("wam").checked) localStorage.setItem("whakamoleScore", 0)
     window.location.reload()
 }
