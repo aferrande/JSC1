f7ee6a7d7e7b37715a296bf260636692a7d098da
Arthur <arthur.ferrande@gmail.com>
2022-06-23 16:59:39 -0300

    finalizado e corrigido

---
+++ js/pokemon.js
@@ -0,0 +1,50 @@
+// const pokemon = { 
+//     method: "GET",
+//     // headers: { "Accept": "application/json" }
+// }
+
+const Reset = () => {
+    let pokemonName = getById("pokemonSearch").value;
+    if(pokemonName.length === 0){
+            getById("pokemon").removeAttribute("style");
+            getById("pokemonId").innerText = "";
+            getById("pokemonName").innerText = "";
+            getById("pokemonHeight").innerText = "";
+            getById("pokemonWeight").innerText = "";
+            getById("pokemonHp").innerText = "";
+            getById("pokemonType").innerText = "";
+            getById("pokemonAtk").innerText = "";
+            getById("pokemonDef").innerText = "";
+            getById("noresult").innerText = "Encontre outros pokemons aqui.";
+            console.log(pokemon)}
+}
+
+const getPokemonInfo = () => {
+    let pokemonName = getById("pokemonSearch").value;
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`).then(d => {
+            d.json().then(pokemon => 
+        {
+                getById("pokemon").style.backgroundImage = `url(${pokemon.sprites.other.dream_world.front_default})`;
+                getById("pokemonId").innerText = pokemon.id;
+                getById("pokemonName").innerText = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+                getById("pokemonHeight").innerText = pokemon.height;
+                getById("pokemonWeight").innerText = pokemon.weight;
+                getById("pokemonHp").innerText = pokemon.stats[0].base_stat;
+                getById("pokemonType").innerText = pokemon.types[0].type.name;
+                getById("pokemonAtk").innerText = pokemon.stats[1].base_stat;
+                getById("pokemonDef").innerText = pokemon.stats[2].base_stat;
+                getById("noresult").setAttribute("href", 'https://pokemondb.net/pokedex/national');
+                getById("noresult").innerText = "Encontre outros pokemons aqui.";   
+                })
+                console.log(pokemon)
+                // if (fetch() = false) {getById("noresult").innerText = "Pokemon não encontrado. Busque aqui"}
+                
+            }).catch(err => {console.log(err); })
+    Reset()  
+}
+
+
+
+
+// display a message whenever no results are found
+
